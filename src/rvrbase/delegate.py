04529c4d23a09993b84780bb2762cd2cbc10b4f1
Robert van Reems <robertreems@gmail.com>
2022-04-27 18:41:29 +0200

    copied all functionalities into new design pattern.

--- src/rvrbase/delegate.py
+++ src/rvrbase/delegate.py
@@ -1,22 +1,62 @@
 ﻿from platform import node
+import json
+import sys
 
 from dbus import ValidationException
 from rvrbase import rvrconfig_delegate
+import rvrbase
 from .new_notification_api import My_new_notification_api
-from rvrbase.constants import NOTIFY_APPLICATION_EVENT, VALID_MESSAGE_TYPES
+from .new_azure_logger_api import My_azure_logger_api
+from rvrbase.constants import LOG_TYPE_APPLICATION_EVENT, NOTIFY_APPLICATION_EVENT, VALID_MESSAGE_TYPES
+
+# todo put it all in try / except
 
 
 class Mydelegate(rvrconfig_delegate.Rvrconfig):
-    
+
     def __init__(self, path):
         super().__init__(path)
 
         self.notifcation_api = My_new_notification_api()
+        self.azure_logger_api = My_azure_logger_api()
+        self.workspace_id = self.q1('workspace_id')
+        self.workspace_prim_key = self.q1('primary_key')
 
     def send_browser_notification(self, message, type):
         if type not in VALID_MESSAGE_TYPES:
             raise ValidationException(
                 f'{type} is not in {VALID_MESSAGE_TYPES}.')
 
-        self.notifcation_api.send_browser_notification(NOTIFY_APPLICATION_EVENT.format(type=type, node=node(), message=message))
-        
+        self.notifcation_api.send_browser_notification(
+            NOTIFY_APPLICATION_EVENT.format(type=type, node=node(), message=message))
+
+    def send_az_app_event(self, type, message):
+        if type not in VALID_MESSAGE_TYPES:
+            raise ValidationException(
+                f'{type} is not in {VALID_MESSAGE_TYPES}.')  # todo use constants.
+
+        body = {
+            "hostname": node(),
+            "script_path": sys.argv[0],
+            "arguments": sys.argv[1:],
+            "type": type,
+            "message": message,
+            "rvrbase_version": rvrbase.__version__
+        }
+
+        body_json = json.dumps(body)
+
+        self.azure_logger_api.post_data(
+            body_json, LOG_TYPE_APPLICATION_EVENT, self.workspace_id, self.workspace_prim_key)
+
+    def send_az_metric(self, log_type, metric_name, value):
+        body = {
+            "hostname": node(),
+            "script_path": sys.argv[0],
+            "metric_name": metric_name,
+            "value": value,
+            "rvrbase_version": rvrbase.__version__
+        }
+
+        body_json = json.dumps(body)
+        self.azure_logger_api.post_data(body_json, log_type, self.workspace_id, self.workspace_prim_key)
---
+++ src/rvrbase/new_azure_logger_api.py
@@ -0,0 +1,68 @@
+﻿# from https://medium.com/slalom-build/reading-and-writing-to-azure-log-analytics-c78461056862
+
+import json
+import sys
+from dbus import ValidationException
+import requests
+import hashlib
+import hmac
+import base64
+import logging
+import datetime
+from platform import node
+import rvrbase
+from .constants import CONFIG_FILE, ERR_POST_DATA, LOG_TYPE_APPLICATION_EVENT, MSG_POST_DATA_SUCCESS, VALID_MESSAGE_TYPES
+
+
+class My_azure_logger_api():
+    def _build_signature(self, customer_id, shared_key, date, content_length, method, content_type,
+                         resource):
+        # Returns authorization header which will be used when sending data into Azure Log
+        # Analytics
+
+        x_headers = 'x-ms-date:' + date
+        string_to_hash = method + "\n" + \
+            str(content_length) + "\n" + content_type + \
+            "\n" + x_headers + "\n" + resource
+        bytes_to_hash = bytes(string_to_hash, 'UTF-8')
+        decoded_key = base64.b64decode(shared_key)
+        encoded_hash = base64.b64encode(hmac.new(
+            decoded_key, bytes_to_hash, digestmod=hashlib.sha256).digest()).decode('utf-8')
+        authorization = "SharedKey {}:{}".format(customer_id, encoded_hash)
+        return authorization
+
+    def post_data(self, body, log_type, workspace_id, workspace_prim_key):
+        """Sends payload to Azure Log Analytics Workspace
+
+        Keyword arguments:
+        customer_id -- Workspace ID obtained from Advanced Settings
+        shared_key -- Authorization header, created using build_signature
+        body -- payload to send to Azure Log Analytics
+        log_type -- Azure Log Analytics table name
+        """
+
+        method = 'POST'
+        content_type = 'application/json'
+        resource = '/api/logs'
+        rfc1123date = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+        content_length = len(body)
+        signature = self._build_signature(
+            workspace_id, workspace_prim_key, rfc1123date, content_length, method, content_type,
+            resource)
+
+        uri = 'https://' + workspace_id + '.ods.opinsights.azure.com' + \
+            resource + '?api-version=2016-04-01'
+
+        headers = {
+            'content-type': content_type,
+            'Authorization': signature,
+            'Log-Type': log_type,
+            'x-ms-date': rfc1123date
+        }
+
+        response = requests.post(uri, data=body, headers=headers)
+        if (response.status_code >= 200 and response.status_code <= 299):
+            logging.info(MSG_POST_DATA_SUCCESS.format(body=body))
+        else:
+            raise RuntimeError(ERR_POST_DATA.format(
+                status_code=response.status_code))
