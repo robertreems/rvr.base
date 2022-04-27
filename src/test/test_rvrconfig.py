beaa138e2716a925ce102102e54c7121ee29dcef
Robert van Reems <robertreems@gmail.com>
2022-04-27 13:38:54 +0200

    more tests

--- src/test/test_rvrconfig.py
+++ src/test/test_rvrconfig.py
@@ -1,14 +1,20 @@
 import unittest
 import rvrbase
 
-# Test query configfile
 
+class Testrvrconfig(unittest.TestCase):
 
-class TestRun(unittest.TestCase):
+    def test_rvrconfig_wrong_file(self):
+        with self.assertRaises(FileNotFoundError):
+            rvrbase.Rvrconfig('filedoesnotexist.ini')
 
-    def test_rvrconfig_q1(self):
+    def test_rvrconfig_q1_success(self):
         config = rvrbase.Rvrconfig('src/test/testdata/config.ini')
         self.assertEqual(config.q1('workspace_id'), 'some_id')
+
+    def test_rvrconfig_q1_non_existing_key(self):
+        config = rvrbase.Rvrconfig('src/test/testdata/config.ini')
+        self.assertEqual(config.q1('keydoesnotexist'), None)
 
 
 if __name__ == '__main__':
