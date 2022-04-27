f73e0b05f8ed968f04f623dedf373eaba85ca43a
Robert van Reems <robertreems@gmail.com>
2022-04-27 16:27:26 +0200

    add version to about

--- src/rvrbase/__init__.py
+++ src/rvrbase/__init__.py
@@ -1,4 +1,5 @@
-from rvrbase.loganalyticslogger import * # noqa
-from rvrbase.rvrconfig import * # noqa
+from rvrbase.logger_api import * # noqa
+from rvrbase.config import * # noqa
 from rvrbase.rvrlogger import * # noqa
 from rvrbase.constants import * # noqa
+from rvrbase.__about__ import (__version__,)
